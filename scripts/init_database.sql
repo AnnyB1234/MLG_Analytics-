a1d2ce657777f4cefdd05d946d4514cf6245af6e
Anurag Bhardwaj <anu281005b@gmail.com>
2026-08-08 12:05:56 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -3,12 +3,12 @@
 Create Database and Schemas
 =============================================================
 Script Purpose:
-    This script creates a new database named 'meridian_logistics_dwh' after checking if it already exists. 
+    This script creates a new database named 'ShopKartDW' after checking if it already exists. 
     If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas 
     within the database: 'bronze', 'silver', and 'gold'.
 	
 WARNING:
-    Running this script will drop the entire 'meridian_logistics_dwh' database if it exists. 
+    Running this script will drop the entire 'ShopKartDW' database if it exists. 
     All data in the database will be permanently deleted. Proceed with caution 
     and ensure you have proper backups before running this script.
 */
@@ -17,18 +17,18 @@ USE master;
 GO
 
 -- Drop and recreate the 'meridian_logistics_dwh' database
-IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'meridian_logistics_dwh')
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'ShopKartDW')
 BEGIN
-    ALTER DATABASE meridian_logistics_dwh SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
-    DROP DATABASE meridian_logistics_dwh;
+    ALTER DATABASE ShopKartDW SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE ShopKartDW;
 END;
 GO
 
 -- Create the 'meridian_logistics_dwh' database
-CREATE DATABASE meridian_logistics_dwh;
-GO
+CREATE DATABASE ShopKartDW;
+GO                                 ------- Go: Separate batches when working with multiple SQL statements
 
-USE meridian_logistics_dwh;
+USE ShopKartDW;
 GO
 
 -- Create Schemas
